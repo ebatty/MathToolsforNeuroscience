80d6b243029e86a782cb7a0b8e641e92b67bcd7e
Ella Batty <erb2180@columbia.edu>
2022-10-25 11:54:42 -0400

    Update process_files.py

--- ci/process_files.py
+++ ci/process_files.py
@@ -21,7 +21,7 @@ def main():
 
 
 def pre_process_notebook(file_path):
-
+    print(file_path)
     with open(file_path, encoding="utf-8") as read_notebook:
         content = json.load(read_notebook)
 
